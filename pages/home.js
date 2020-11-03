cc69c625b0e124aeacdf91b6dfcaac6dcd4e1290
Rahmatullaah Boluwatife Ibraheem <ibraheemrahmatullah@gmail.com>
2020-11-03 10:37:20 +0100

    ...

--- pages/home.js
+++ pages/home.js
@@ -1,35 +1,3 @@
-const demo = {
-
-    $message: null,
-
-    fetchCurrent: async () => {
-        try {
-
-            const response = await fetch
-('https://api.openweathermap.org/data/2.5/weather?q=New York&exclude=minutely,hourly,daily&mode=html&appid=324ac8652cb0e662c90f3678aa7a88cf');
-
-            const json = await response.text();
-            // const message = JSON.stringify(json);
-
-
-            demo.$message.innerHTML = json;
-           
-            
-        } catch (e) {
-            console.log(e);
-        }
-   
-    },
-    
-        load: () => {
-        demo.$message = document.querySelector('#message');
-    }
-};
-
-window.addEventListener('DOMContentLoaded', demo.load);
-
-    
-
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', async () => {
             try {
